353933cec656dacd7e9b5896b2889480b335a99c
echevalier <estebanchevalier@gmail.com>
2022-07-22 07:40:18 -0300

    Add template mail

--- components/registration/services/registrationService.js
+++ components/registration/services/registrationService.js
@@ -18,13 +18,18 @@ class Registration {
 
     async postRegistration(req) {
         try {
-            //console.log(req.body)
-            sendMail('Nuevo Ingreso', JSON.stringify(req.body, null, 2))
+            // { "username": "Hulk", "password": "123456", "email": "hulk@gmail.com", "firstname": "Bruce", "lastname": "Banner", "address": "La Florida 22", "phone": "+54935123412", "age": "1954-02-12" }
+            let template = `<table border="1"><tr>`;
+            Object.keys(req.body).forEach( key => template += `<td>${key[0].toUpperCase() + key.substring(1).toLowerCase()}</td>`);
+            template += `</tr><tr>`;
+            Object.values(req.body).forEach( value => template += `<td>${value}</td>`);
+            template += `</tr></table>`;
+
+            sendMail('Nuevo Ingreso', template)
             const token = jwt.sign({ data: req.user }, PRIVATE_KEY, { expiresIn: '24h' });
 
             let response = {status: 'OK', data: req.user, jwt: token, message: 'User Register OK'}
-
-
+            
             //let response = { status: 'OK', message: 'User Register OK' }
             return response
 
